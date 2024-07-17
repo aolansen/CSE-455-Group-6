a25ff9702220b8eebd6123743a3679adfa18f281
Zac Sanger <sangerzachary@gmail.com>
2024-07-17 08:00:03 -0400

    Created Preproccesing the Pixels Dataframe -> List -> 48x48 array -> Img Array 2d -> Img Array 3d

--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,28 @@
 import numpy as np
 import pandas as pd
 
-faces = pd.read_csv("fer2013/fer2013/fer2013.csv")
+
+
+faces = pd.read_csv("fer2013.csv")
 print(faces.head())
+
+#Turn DataFram into List
+pixels = faces["pixels"].tolist()
+emotion = faces["emotion"].tolist()
+Usage = faces["Usage"].tolist()
+
+#Turning the list into an array 48X48 Function
+def str_to_img(pixels):
+        image =np.array(pixels.split(), dtype='float32')
+        return image.reshape(48, 48)
+
+#Going through the pixels and makes them into images
+image_list=[]
+for pixel in pixels:
+    img = str_to_img(pixel)
+    image_list.append(img)
+
+#2 Dimensional Array
+images = np.array(image_list)
+#Making into 3 dimensional Array for use in CNN
+images = np.expand_dims(images, -1) 
