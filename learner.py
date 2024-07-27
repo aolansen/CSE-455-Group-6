a4966ee4c06f1252e7436cf5ee6bdff1f678cb2b
Zac Sanger <sangerzachary@gmail.com>
2024-07-26 23:16:06 -0400

    Switched to resnet model and add 3 fully connected layers, Added a progress bar and reworked Preprosscor

--- learner.py
+++ learner.py
@@ -1,29 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+from torchvision.models import ResNet18_Weights
+import time
+from tqdm import tqdm
+from torchinfo import summary
+
 import preprocessor
-import math
 
-data = preprocessor.outputter()
-#print('first entry:', data[0])
+class CustomHead(nn.Module):
+    def __init__(self, featr):
+        super(CustomHead, self).__init__()
+        self.fc1 = nn.Linear(featr, 512)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(256, 7)
 
-# kernels defined here. 
-kernelOne = [[0, 1, 2], [-1, 0, 1], [-2, -1, 0]] # pls be ok size (maybe 5x5 easier on computer)
-kernelTwo = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
-kernelThree = [[0, 1, 2], [-1, 0, 1], [-2, -1, 0]]
-kernelFour = [[1, 0, -1], [0, 0, 0], [-1, 0, 1]]
-kernelFive = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]]
-kernelArr = [kernelOne, kernelTwo, kernelThree, kernelFour, kernelFive]
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = nn.ReLU()(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = nn.ReLU()(x)
+        x = self.dropout2(x)
+        x = self.fc3(x)
+        return x
 
-# padding, stride, and output size defined here
-padding = float((len(kernelOne) - 1)/2) # equal to 1 (defined this way in case we change kernel dimensionality)
-stride = 3 # larger stride = faster (reduce size in lower layers)
-filteredSize = math.floor((len(data[0]) + 2 * padding - len(kernelOne))/stride + 1)
-#print('output size:', filteredSize)
 
-def convolute(): 
-    transformer = []
-    for i in range(0, len(data[0]), stride): # are we going to have to use thrice nested loops here? :( only looping in first entry for now
-        # need to mind padding but getting type error when subtracting from len(data[0])
-        entry = 0
-        #print(data[0][i])
-        # there's probably predefined numpy functions for handling what we need to do with filtering, fact it's in C should help with runtime
 
-convolute()
+
+# Load a pre-trained ResNet model
+weights = ResNet18_Weights.DEFAULT
+model = models.resnet18(weights=weights)
+featr = model.fc.in_features
+model.fc = CustomHead(featr)
+
+
+summary(model, input_size=(preprocessor.batch_size, preprocessor.channels, preprocessor.img_width, preprocessor.img_height))
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=.001)
+
+# Training loop
+num_epochs = 20
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+model.load_state_dict(torch.load('emotion_detector_epoch_2l.pth'))
+for epoch in range(num_epochs):
+    print(f'Epoch: {epoch}')
+    model.train()
+    running_loss = 0.0
+
+    start_time = time.time()
+
+    # Use tqdm for progress bar
+    with tqdm(total=len(preprocessor.dataloaders['train']), desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch') as pbar:
+        for batch_idx, (inputs, labels) in enumerate(preprocessor.dataloaders['train']):
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+
+            pbar.set_postfix(loss=loss.item())
+            pbar.update(1)
+
+    epoch_loss = running_loss / len(preprocessor.dataloaders['train'].dataset)
+    elapsed_time = time.time() - start_time
+    print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}, Time: {elapsed_time:.2f}s')
+
+    # Save checkpoint every epoch
+    torch.save(model.state_dict(), f'emotion_detector_epoch_{epoch + 1}.pth')
--- preprocessor.py
+++ preprocessor.py
@@ -1,38 +1,68 @@
+import torchvision
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
 #import time
 #start = time.time()
 
-faces = pd.read_csv("fer2013/fer2013/fer2013.csv")
-#faces = pd.read_csv("fer2013")
-#print(faces.head())
+train = 'C:/Users/sange/Downloads/archive/train'
+test = 'C:/Users/sange/Downloads/archive/test'
 
-#Turn DataFram into List
-pixels = faces["pixels"].tolist()
-emotion = faces["emotion"].tolist()
-usage = faces["Usage"].tolist()
+img_width, img_height, channels = 224, 224, 3
+batch_size = 1024
 
-#Turning the list into an array 48X48 Function
-def str_to_img(pixels):
-        image = np.array(pixels.split(), dtype='float32')
-        return image.reshape(48, 48)
+nb_train_samples = 28709
+nb_test_samples = 7178
 
-#Going through the pixels and makes them into images
-image_list=[]
-for pixel in pixels:
-    img = str_to_img(pixel)
-    image_list.append(img)
+data_transforms = {
+    'train': transforms.Compose([
 
-#2 Dimensional Array
-images = np.array(image_list)
-#Making into 3 dimensional Array for use in CNN
-#images = np.expand_dims(images, -1) # commenting out for now
+        transforms.Resize((img_width, img_height)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.ToTensor(),
+        transforms.RandomVerticalFlip(p=0.005),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-#finish = time.time()
-#print("This took", finish - start, "seconds")
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((img_width, img_height)),
+        transforms.ToTensor(),  # Convert PIL image to tensor
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((img_width, img_height)),
+        transforms.ToTensor(),  # Convert PIL image to tensor
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+}
 
-def outputter():
-      #print(images.ndim)
-      return images
+image_datasets = {
+    'train': datasets.ImageFolder(train, data_transforms['train']),
+    'test': datasets.ImageFolder(test, data_transforms['test']),
+}
 
-outputter()
+# Create data loaders
+dataloaders = {
+    'train': DataLoader(image_datasets['train'], batch_size=batch_size, shuffle=True, drop_last=True),
+    'test': DataLoader(image_datasets['test'], batch_size=batch_size, shuffle=False, drop_last=True),
+}
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#
+#def imshow(img):
+   # img = img / 2 + 0.5     # unnormalize
+  #  npimg = img.numpy()
+ #   plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#    plt.show()
+
+
+# get some random training images
+#dataiter = iter(dataloaders['train'])
+#images, labels = next(dataiter)
+
+# show images
+#imshow(torchvision.utils.make_grid(images[23]))
